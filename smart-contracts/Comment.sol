82f9c33adc154407b5537f1f55fa24b36a57904d
Slick Rick <phx805@gmail.com>
2023-12-09 16:38:59 -0700

    Toheeb

---
+++ smart-contracts/Comment.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract Comment {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the contract owner");
+        _;
+    }
+
+    function sendPayment(address payable recipient) external onlyOwner payable {
+        require(recipient != address(0xA7EaA380968CC93C42C510F8424d163a0E76256D), "Invalid recipient address");
+        require(msg.value > 0, "Invalid payment amount");
+
+        recipient.transfer(msg.value);
+    }
+
+    receive() external payable {
+        // Fallback function to receive payments
+    }
+}
