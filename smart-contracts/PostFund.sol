867ead2c71dbb73c3337125a517707f0b62b592e
Slick Rick <94326179+phx805@users.noreply.github.com>
2023-12-11 03:26:27 -0700

    Update PostFund.sol

--- smart-contracts/PostFund.sol
+++ smart-contracts/PostFund.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.18;
 //0x17F762DfA1C14744eEe85Ddd3B5980A89a17040d polygon mumbai
 //0x88dD0D6365A19394441dE214c684F768cc604f71 sepolia
-//0x0455B0Be9E2e58aCc011Aa7026e45836402f69F8 avalanche tesnet
+//0xf47c98abA1a4c4eB778991AeE7Ea889a977fEA3E avalanche tesnet
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
